b2b48a40bc43d232885645eef0e9b05edeabdd60
Julian Nadeau <julian@jnadeau.ca>
2016-09-28 00:13:32 -0400

    Fix assets load paths

--- lib/dashboard/engine.rb
+++ lib/dashboard/engine.rb
@@ -2,11 +2,27 @@ module Dashboard
   class Engine < Rails::Engine # :nodoc:
     isolate_namespace Dashboard
 
-    config.to_prepare do
-      Rails.application.config.assets.precompile += %w(
-        toasts.js
-        toasts.css
-      )
-    end
+    config.assets.paths << File.join(
+      Gem.loaded_specs['bootstrap'].full_gem_path,
+      'assets',
+      'javascripts'
+    )
+    config.assets.paths << File.join(
+      Gem.loaded_specs['bootstrap'].full_gem_path,
+      'assets',
+      'stylesheets'
+    )
+    config.assets.paths << File.join(
+      Gem.loaded_specs['font-awesome-rails'].full_gem_path,
+      'app',
+      'assets',
+      'stylesheets'
+    )
+    config.assets.paths << File.join(
+      Gem.loaded_specs['font-awesome-rails'].full_gem_path,
+      'app',
+      'assets',
+      'fonts'
+    )
   end
 end
